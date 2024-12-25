cb8184a7028dc1c593146b0c93bfafc50944c04a
Bilal Ajaha <bilalajaha@gmail.com>
2024-12-25 11:53:49 +0100

    Connecting to database and building homepage

---
+++ Address-Book/db.php
@@ -0,0 +1,22 @@
+<?php
+
+$nom = $_POST['nom'];
+$telephone = $_POST['telephone'];
+$email = $_POST['email'];
+$adress = $_POST['adress'];
+
+// Database Connection
+$servername = 'localhost';
+$username = 'root';
+$password = 'root';
+$dbname = 'adressbook';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO book (nom, telephone, email, adress) VALUES ('$nom', '$telephone', '$email', '$adress')";
+
+?>
--- Address-Book/index.php
+++ Address-Book/index.php
@@ -6,6 +6,20 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="inde.php" method="post">
+        <label for="nom">Nom</label>
+        <input type="text" name="nom">
+
+        <label for="nom">Telephone</label>
+        <input type="text" name="telephone">
+
+        <label for="nom">Email</label>
+        <input type="text" name="email">
+
+        <label for="nom">Adress</label>
+        <input type="text" name="adress">
+    </form>
+<?php
+?>
 </body>
 </html>
